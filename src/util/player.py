846ab45101737b9d2648a600a1a3ba15b708bac8
EKYS12 <syasser126@gmail.com>
2024-10-22 11:36:19 -0400

    Tried to fix player card print out, but am now being side tracked by my neovim config and ability to copy-paste to system clipboard

--- src/util/player.py
+++ src/util/player.py
@@ -1,9 +1,21 @@
 '''
 This is the Player class that will be used to represent players in the game. Players will have a hand that contains the cards that the player owns. The class contains a dealer flag that determines if they are the dealer or not.
 '''
+
 class Player:
     def __init__(self, dealer=False):
 
         self.hand = []
         self.dealer = dealer
 
+    def __str__(self):
+        '''
+        String print out representation of the players hand.
+        '''
+
+        hand_str = ""
+
+        for card in self.hand:
+            hand_str = hand_str + f'\n{str(card)}'
+
+        return hand_str
