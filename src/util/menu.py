8e761d0b77f7d9de607c61531a15b840807296ee
EKYS12 <syasser126@gmail.com>
2024-10-24 12:43:37 -0400

    Worked a bit on menu, trying to decide best way to have menu call and start a game.

--- src/util/menu.py
+++ src/util/menu.py
@@ -1,1 +1,27 @@
-print('This is menu')
+import sys
+import game
+
+print('Welcome to Black Jack')
+
+print('Would you like to play?')
+
+print('\n1. Yes, start a game')
+print('\n2. No, exit')
+
+not_valid_input = True
+
+while not_valid_input:
+    user_input = int(input('Choice: '))
+
+    if user_input in [1,2]:
+        not_valid_input = False
+
+    else:
+        print('Invalid input, try again')
+
+if user_input == 1:
+    game
+
+
+if user_input == 2:
+    sys.exit()
